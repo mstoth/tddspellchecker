39aa07e265e00c50f638a03ddc646b446306e06c
Michael Toth <michaeltoth@Michaels-iMac.local>
2017-11-05 07:05:59 -0500

    distance passes

--- spell-checker/src/spell_checker/core.clj
+++ spell-checker/src/spell_checker/core.clj
@@ -5,7 +5,7 @@
 
 (def words (set (map str/trim (str/split-lines (slurp "resources/wordsEn.txt")))))
 (defn correct? [w] (if (contains? words w) (println "correct!") (println "did you mean xy?")))
-(defn distance [x y] nil)
+(defn distance [x y] (StringUtils/getLevenshteinDistance x y))
 (defn -main
   "I print out 'correct!' if w is in words"
   [& args]
